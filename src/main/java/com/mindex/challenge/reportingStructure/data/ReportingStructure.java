790ed0fdb955973ec7fc9aaf33386e26fabf5e5d
Max Savard <28509547+TravelinMax@users.noreply.github.com>
2023-05-15 16:06:54 -0400

    Added comment

--- src/main/java/com/mindex/challenge/reportingStructure/data/ReportingStructure.java
+++ src/main/java/com/mindex/challenge/reportingStructure/data/ReportingStructure.java
@@ -2,7 +2,9 @@ package com.mindex.challenge.reportingStructure.data;
 
 import com.mindex.challenge.employee.data.Employee;
 
-/* I might refactor this class and @Compensation to be immutable */
+/* I might refactor this class and @Compensation to be immutable
+   it forces you to use the parameterized constructor which results in cleaner code imo
+ */
 public class ReportingStructure {
     private Employee employee;
 
